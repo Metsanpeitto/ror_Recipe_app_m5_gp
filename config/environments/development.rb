fa52b35924338ebd6bd863911be7c1e2080a365e
Metsanpeitto <abraham@waldenberginc.com>
2021-11-14 23:02:34 +0100

    Add bootstrap and devise views

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -73,4 +73,23 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+
+config.action_mailer.delivery_method = :smtp
+host = 'localhost:3000' #replace with your own url
+config.action_mailer.default_url_options = { host: host }
+config.action_mailer.default_options = { from: ENV["GMAIL_USERNAME"] }
+config.action_mailer.perform_deliveries = true
+config.action_mailer.default :charset => "utf-8"
+#config.action_mailer.default_url_options = Rails.application.default_url_options
+# SMTP settings for gmail
+config.action_mailer.smtp_settings = {
+  :address => 'smtp.gmail.com',
+  :port => 587,
+  :domain => 'smtp.gmail.com',
+  :user_name => ENV["GMAIL_USERNAME"],
+  :password => ENV["GMAIL_PASSWORD"],
+  :authentication => 'plain',
+  :enable_starttls_auto => true
+}   
 end
